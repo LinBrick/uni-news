2c3f86a692c4bc1a6db9ecdc3132995215490e93
lwy-lenovo <527563266@qq.com>
2020-08-14 16:15:06 +0800

    意见反馈页面制作

--- utils/index.js
+++ utils/index.js
@@ -23,3 +23,15 @@ export const parseTime = (time) => {
 	})
 	return strTime
 }
+
+// 生成随机数
+export function randomString(len) {
+  len = len || 32
+  const $chars = 'ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678'
+  const maxPos = $chars.length
+  let pwd = ''
+  for (let i = 0; i < len; i++) {
+    pwd += $chars.charAt(Math.floor(Math.random() * maxPos))
+  }
+  return pwd
+}
